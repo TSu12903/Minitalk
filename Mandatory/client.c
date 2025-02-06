0634794a08bd08b900dee9288f2898c3d9584ac6
Timothy Cybak <tcybak@2F5.42angouleme.fr>
2025-02-06 17:39:35 +0100

    up

--- Mandatory/client.c
+++ Mandatory/client.c
@@ -6,29 +6,39 @@
 /*   By: tcybak <tcybak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 16:38:45 by tcybak            #+#    #+#             */
-/*   Updated: 2025/02/04 16:39:47 by tcybak           ###   ########.fr       */
+/*   Updated: 2025/02/05 16:58:27 by tcybak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void    convert_Ascii(char av)
+void    convert_Ascii(char character, int pid)
 {
-    int size_message;
-    int i;
+    int     size_message;
+    int     i;
+    char    str;
 
     i = 7;
-    size_message = ft_strlen(&av);
+    size_message = ft_strlen(&character);
+    str = character;
     while (i >= 0)
     {
-        ft_printf("%d", (av >> i) & 1);
+        str = (character >> i) & 1;
+        ft_printf("%d", str);
+        if (str % 2 == 0)
+            kill(pid, SIGUSR1);
+        else
+            kill(pid, SIGUSR2);
         i--;
     }
 }
 
 int main(int ac, char **av)
 {
+    int pid_serveur;
+
+    pid_serveur = ft_atoi(av[1]);
     if (ac != 3)
         return (write(2, "Error arguments\n", 16));
-    convert_Ascii(*av[2]);
+    convert_Ascii(*av[2], pid_serveur);
 }
--- Mandatory/serveur.c
+++ Mandatory/serveur.c
@@ -6,17 +6,40 @@
 /*   By: tcybak <tcybak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 16:43:32 by tcybak            #+#    #+#             */
-/*   Updated: 2025/02/04 15:49:47 by tcybak           ###   ########.fr       */
+/*   Updated: 2025/02/06 17:38:49 by tcybak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void signal_handler(int signum)
+{
+	static unsigned char current_char = 0;
+	int	bits = 0;
+	
+	current_char |= (signal == SIGUSR1);
+	bits++;
+	if (bits == 8)
+	{
+		if (current_char == 0)
+			ft_printf("\n");
+		else
+			ft_printf("%c", current_char);
+		bits = 0;
+		current_char = 0;
+	}
+	else
+		current_char <<= 1;
+}
+
 int main(void)
 {
-    ft_printf("Mon PID est : %d\n", getpid());
-    pause();
-    if (pause() == -1)
-        return (0);
-    return (0);
+	struct sigaction usr1;
+
+	ft_printf("Mon PID est : %d\n", getpid());
+	ft_bzero(&usr1, sizeof(char));
+	sigaction(SIGUSR2, &usr1, signal_handler);
+	while(1)
+		pause();
+	return (0);
 }
