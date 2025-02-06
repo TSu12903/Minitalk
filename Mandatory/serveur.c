7b317d6d5f5d0540dd18f21b43126ca347ace4a9
Timothy Cybak <tcybak@2F5.42angouleme.fr>
2025-02-06 18:42:14 +0100

    work need upgrade

--- Mandatory/serveur.c
+++ Mandatory/serveur.c
@@ -6,25 +6,25 @@
 /*   By: tcybak <tcybak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 16:43:32 by tcybak            #+#    #+#             */
-/*   Updated: 2025/02/06 17:38:49 by tcybak           ###   ########.fr       */
+/*   Updated: 2025/02/06 18:39:18 by tcybak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void signal_handler(int signum)
+void	signal_handler(int signal)
 {
-	static unsigned char current_char = 0;
-	int	bits = 0;
-	
-	current_char |= (signal == SIGUSR1);
+	static unsigned char current_char;
+	static int	bits;
+
+	current_char |= (signal == SIGUSR2);
 	bits++;
 	if (bits == 8)
 	{
 		if (current_char == 0)
 			ft_printf("\n");
 		else
-			ft_printf("%c", current_char);
+			ft_printf("%c",current_char);
 		bits = 0;
 		current_char = 0;
 	}
@@ -35,11 +35,18 @@ void signal_handler(int signum)
 int main(void)
 {
 	struct sigaction usr1;
+	struct sigaction usr2;
 
 	ft_printf("Mon PID est : %d\n", getpid());
-	ft_bzero(&usr1, sizeof(char));
-	sigaction(SIGUSR2, &usr1, signal_handler);
+	ft_bzero(&usr1, sizeof(usr1));
+	usr1.sa_handler = &signal_handler;
+	sigaction(SIGUSR1, &usr1, NULL);
+	
+	ft_bzero(&usr2, sizeof(usr2));
+	usr2.sa_handler = &signal_handler;
+	sigaction(SIGUSR2, &usr2, NULL);
+
 	while(1)
-		pause();
+		continue ;
 	return (0);
 }
