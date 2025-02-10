a2f4e2c5957f7ee260fd0af5cfbddf15c4c37bb5
Timothy Cybak <tcybak@2B3.42angouleme.fr>
2025-02-10 15:13:14 +0100

    good no bonus

--- Mandatory/client.c
+++ Mandatory/client.c
@@ -6,18 +6,18 @@
 /*   By: tcybak <tcybak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 16:38:45 by tcybak            #+#    #+#             */
-/*   Updated: 2025/02/10 13:00:03 by tcybak           ###   ########.fr       */
+/*   Updated: 2025/02/10 14:57:08 by tcybak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../Library/libft/libft.h"
+#include "../Library/libft/libft.h"
 #include <signal.h>
 #include <unistd.h>
 
-void    convert_Ascii(char character, int pid)
+static void	convert_ascii(char character, int pid)
 {
-	int     i;
-	char    str;
+	int		i;
+	char	str;
 
 	i = 7;
 	while (i >= 0)
@@ -28,14 +28,14 @@ void    convert_Ascii(char character, int pid)
 		else
 			kill(pid, SIGUSR2);
 		i--;
-		usleep(500);
+		usleep(700);
 	}
 }
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-	int pid_serveur;
-	int i;
+	int	pid_serveur;
+	int	i;
 
 	i = 0;
 	pid_serveur = ft_atoi(av[1]);
@@ -48,14 +48,14 @@ int main(int ac, char **av)
 	}
 	while (av[2][i])
 	{
-		convert_Ascii(av[2][i], pid_serveur);
+		convert_ascii(av[2][i], pid_serveur);
 		i++;
 	}
 	i = 8;
 	while (i > 0)
 	{
 		kill(pid_serveur, SIGUSR1);
-		usleep(500);
+		usleep(700);
 		i--;
 	}
 	return (0);
--- Mandatory/serveur.c
+++ Mandatory/serveur.c
@@ -6,36 +6,36 @@
 /*   By: tcybak <tcybak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 16:43:32 by tcybak            #+#    #+#             */
-/*   Updated: 2025/02/10 13:42:40 by tcybak           ###   ########.fr       */
+/*   Updated: 2025/02/10 15:07:45 by tcybak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../Library/libft/libft.h"
+#include "../Library/libft/libft.h"
 #include <signal.h>
-#include <unistd.h>
 
-static void	allocate_messages(char current_char, char **str)
+char	*g_str;
+
+static void	allocate_messages(char current_char, char **g_str)
 {
 	char	*tmp;
 	char	tmp2[2];
-	
+
 	tmp2[0] = current_char;
 	tmp2[1] = 0;
-	if (*str == 0)
+	if (*g_str == 0)
 		tmp = ft_strdup(tmp2);
 	else
 	{
-		tmp = ft_strjoin(*str, tmp2);
-		free(*str);
+		tmp = ft_strjoin(*g_str, tmp2);
+		free(*g_str);
 	}
-	*str = tmp;
+	*g_str = tmp;
 }
 
 static void	signal_handler(int signal)
 {
 	static unsigned char	current_char;
 	static int				bits;
-	static char				*str;
 
 	current_char |= (signal == SIGUSR2);
 	bits++;
@@ -43,12 +43,12 @@ static void	signal_handler(int signal)
 	{
 		if (current_char == 0)
 		{
-			ft_printf("%s\n", str);
-			free(str);
-			str = NULL;
+			ft_printf("%s\n", g_str);
+			free(g_str);
+			g_str = NULL;
 		}
 		else
-			allocate_messages(current_char, &str);
+			allocate_messages(current_char, &g_str);
 		bits = 0;
 		current_char = 0;
 	}
@@ -58,30 +58,31 @@ static void	signal_handler(int signal)
 
 static void	sigint_handler(int sig)
 {
-	(void)sig;
+	if (sig)
+	{
+		if (g_str)
+			free(g_str);
+	}
 	exit(1);
 }
 
-int main(void)
+int	main(void)
 {
-	struct sigaction usr1;
-	struct sigaction usr2;
-	struct sigaction sigint;
-	
+	struct sigaction	usr1;
+	struct sigaction	usr2;
+	struct sigaction	sigint;
+
 	ft_printf("Mon PID est : %d\n", getpid());
 	ft_bzero(&usr1, sizeof(usr1));
 	usr1.sa_handler = &signal_handler;
 	sigaction(SIGUSR1, &usr1, NULL);
-	
 	ft_bzero(&usr2, sizeof(usr2));
 	usr2.sa_handler = &signal_handler;
 	sigaction(SIGUSR2, &usr2, NULL);
-	
 	ft_bzero(&sigint, sizeof(sigint));
 	sigint.sa_handler = &sigint_handler;
 	sigaction(SIGINT, &sigint, NULL);
-
-	while(1)
+	while (1)
 		pause ();
 	return (0);
 }
